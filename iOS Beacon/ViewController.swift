4b9fbd340696d873a8407678909fe87b6cc004c3
Nicholas Nuing <nicholas.nuing@gmail.com>
2017-10-03 12:28:02 +0800

    Initial commit.

--- iOS Beacon/ViewController.swift
+++ iOS Beacon/ViewController.swift
@@ -1,25 +1,65 @@
 //
 //  ViewController.swift
-//  iOS Beacon
+//  Beaker Beacon
 //
 //  Created by Nicholas Nuing on 03/10/2017.
 //  Copyright © 2017 otakrosak. All rights reserved.
+//  5725E00C-D553-4E09-8473-77AD8EE665EB
 //
 
 import UIKit
+import CoreLocation
+import CoreBluetooth
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate, CBPeripheralManagerDelegate {
+    var localBeacon: CLBeaconRegion!
+    var beaconPeripheralData: NSDictionary!
+    var peripheralManager: CBPeripheralManager!
+    // let locationManager = CLLocationManager()
+    
+    func initLocalBeacon() {
+        if localBeacon != nil {
+            stopLocalBeacon()
+        }
+        
+        let localBeaconUUID = "5725E00C-D553-4E09-8473-77AD8EE665EB"
+        let localBeaconMajor: CLBeaconMajorValue = 123
+        let localBeaconMinor: CLBeaconMinorValue = 456
+        let beaconID = "com.otakrosak.Beaker-Beacon"
+        
+        let uuid = UUID(uuidString: localBeaconUUID)!
+        localBeacon = CLBeaconRegion(proximityUUID: uuid, major: localBeaconMajor, minor: localBeaconMinor, identifier: beaconID)
+        
+        beaconPeripheralData = localBeacon.peripheralData(withMeasuredPower: nil)
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil, options: nil)
+    }
+    
+    func stopLocalBeacon() {
+        peripheralManager.stopAdvertising()
+        peripheralManager = nil
+        beaconPeripheralData = nil
+        localBeacon = nil
+    }
+    
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        if peripheral.state == .poweredOn {
+            peripheralManager.startAdvertising(beaconPeripheralData as! [String: AnyObject]!)
+        } else if peripheral.state == .poweredOff {
+            peripheralManager.stopAdvertising()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        initLocalBeacon()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
